4b558fb44d0a177bfddcae972e93245e1aa1ccb9
Guilherme Namen Pimenta <guilherme.namen@gmail.com>
2018-07-30 10:10:12 -0300

    Update HyperVInventory.ps1
    Change for better performance

--- HyperVInventory.ps1
+++ HyperVInventory.ps1
@@ -1,10 +1,8 @@
-#requires -version 4.0
-
 function Check-Servers {
   Param(
     [parameter(Mandatory=$true)]
     [String[]]
-    $Servers 
+    $Servers
   )
   $computers = [String[]]@()
   $I = 0
@@ -17,8 +15,9 @@ function Check-Servers {
     }
     $I++
     $percentil = ($I / $Servers.Count) * 100
-    Write-Progress -Activity "Testing servers connection..." -Status "$([int]$percentil)% Complete:" -PercentComplete $percentil
+    Write-Progress -Activity "Testing servers connection..." -Status "$([int]$percentil)% Complete:" -PercentComplete $percentil -Id 1
   }
+  Write-Progress -Activity 'Testing servers connection...' -Completed -Id 1
   return $computers
 }
 
@@ -28,77 +27,82 @@ function Get-ServersInformation {
     [String[]]
     $Servers 
   )
-  $wmiObjects = [ordered]@{
+  $wmiObjects = @{
     Win32_Operatingsystem = @{
+      Query = "select InstallDate, Caption, Version, OSArchitecture from Win32_Operatingsystem";
       Param = @(@{Name="Computername";Expression={$_.PSComputername}},"InstallDate","Caption","Version","OSArchitecture");
-      Filter = $null;
-      Sort = $null
+      Sort = $null;
     }
     Win32_Computersystem = @{
-      Param = @("PSComputername","TotalPhysicalMemory","HyperVisorPresent","NumberOfProcessors","NumberofLogicalProcessors", "Domain");
-      Filter = $null;
-      Sort = $null
+      Query = "select TotalPhysicalMemory, HyperVisorPresent, NumberOfProcessors, NumberofLogicalProcessors, Domain, Name, DomainRole from Win32_Computersystem";
+      Param = @("PSComputername","TotalPhysicalMemory","HyperVisorPresent","NumberOfProcessors","NumberofLogicalProcessors", "Domain", "Name", "DomainRole");
+      Sort = $null;
     }
     Win32_Computersystemproduct = @{
+      Query = "select Name, Vendor, uuid from Win32_Computersystemproduct";
       Param = @("PSComputername", "Name", "Vendor", "uuid");
-      Filter = $null;
-      Sort = $null
+      Sort = $null;
     }
     Win32_DiskDrive = @{
+      Query = "select Caption, Model, Size, SerialNumber, DeviceID, InterfaceType from Win32_DiskDrive";
       Param = @("PSComputername", "Caption", "Model", "Size", "SerialNumber", "DeviceID", "InterfaceType");
-      Filter = $null;
-      Sort = $null
+      Sort = $null;
     }
     Win32_NetworkAdapter = @{
+      Query = "select Name, Speed, MACAddress, NetEnabled from Win32_NetworkAdapter where AdapterTypeID = 0 or AdapterTypeID = 9 or ConfigManagerErrorCode = 22";
       Param = @("PSComputername", "Name", "Speed", "MACAddress", "NetEnabled");
-      Filter = "AdapterTypeID = 0 or AdapterTypeID = 9 or ConfigManagerErrorCode = 22";
-      Sort = $null
+      Sort = "Name";
     }
     Win32_NetworkAdapterConfiguration = @{
+      Query = "select MACAddress, DHCPEnabled, DefaultIPGateway, IPAddress, IPSubnet, DNSServerSearchOrder from Win32_NetworkAdapterConfiguration where MACAddress is not null";
       Param = @("PSComputername", "MACAddress", "DHCPEnabled", "DefaultIPGateway", "IPAddress", "IPSubnet", "DNSServerSearchOrder");
-      Filter = "MACAddress is not null";
-      Sort = $null
+      Sort = $null;
     }
     Win32_Service = @{
-      Param = @("PSComputername","Name","Displayname","StartMode","State","StartName","ProcessID");
-      Filter = "State = 'Running'";
-      Sort = "ProcessID"
+      Query = "select Name, Displayname, StartMode, StartName, ProcessID from Win32_Service where State = 'Running'"
+      Param = @("PSComputername","Name", "Displayname", "StartMode", "StartName", "ProcessID");
+      Sort = "ProcessId";
     }
-    Win32_NetworkOpenPort = $null
+#    Win32_Process = @{
+#      Query = "select ProcessID, ParentProcessID from Win32_Process"
+#      Param = @("PSComputername","ProcessID", "ParentProcessID");
+#      Sort = "ProcessId";
+#    }
+#    Process = $null;
+    NetworkOpenPort = $null;
     
   }
-  $wmiValues = [ordered]@{
-    Win32_Operatingsystem = $null;
-    Win32_Computersystem = $null;
-    Win32_Computersystemproduct = $null;
-    Win32_DiskDrive = $null;
-    Win32_NetworkAdapter = $null;
-    Win32_NetworkAdapterConfiguration = $null;
-    Win32_NetworkOpenPort = $null;
-    Win32_Service = $null;
-  }
+  $wmiValues = @{}
   $I = 0
   foreach ($wmiObjectKey in $wmiObjects.Keys) {
     $wmiObject = $wmiObjects[$wmiObjectKey]
-    if ($wmiObjectKey -ne "Win32_NetworkOpenPort") {
+    if (($wmiObjectKey -ne "NetworkOpenPort") -and ($wmiObjectKey -ne "Process")) {
       if ($wmiObject.Sort -eq $null) {
-        $wmiValues[$wmiObjectKey] = gwmi $wmiObjectKey -ComputerName $computers -Filter $wmiObject.Filter | Select-Object $wmiObject.Param
+        $wmiValues[$wmiObjectKey] = Invoke-Command -ComputerName $computers {Get-WmiObject -Query $args[0] } -ArgumentList $wmiObject.Query | Select-Object $wmiObject.Param
+        #$wmiValues[$wmiObjectKey] = gwmi -Namespace root/cimv2 $wmiObjectKey -ComputerName $computers -Filter $wmiObject.Filter | Select-Object $wmiObject.Param | Select-Object $wmiObject.Param
       } else {
-        $wmiValues[$wmiObjectKey] = gwmi $wmiObjectKey -ComputerName $computers -Filter $wmiObject.Filter | Select-Object $wmiObject.Param | Sort $wmiObject.Sort
+        $wmiValues[$wmiObjectKey] = Invoke-Command -ComputerName $computers {Get-WmiObject -Query $args[0]} -ArgumentList $wmiObject.Query | Select-Object $wmiObject.Param | Sort $wmiObject.Sort
+        #gwmi -Namespace root/cimv2 $wmiObjectKey -ComputerName $computers -Filter $wmiObject.Filter | Select-Object $wmiObject.Param | Select-Object $wmiObject.Param | Sort $wmiObject.Sort
       }
-    } else {
+    } elseif  ($wmiObjectKey -eq "NetworkOpenPort"){
       $wmiValues[$wmiObjectKey] = Invoke-Command -ComputerName $computers {c:\windows\system32\netstat.exe -ano}
+    } elseif ($wmiObjectKey -eq "Process") {
+      $wmiValues[$wmiObjectKey] = Get-Process -ComputerName $computers | select Id,ProcessName,Description,Company,Product,ProductVersion,@{N='PSComputerName'; E={$_.MachineName}}
     }
     $I++
     $percentil = ($I / $wmiObjects.Keys.Count) * 100
-    Write-Progress -Activity "Reading data from servers..." -Status "$([int]$percentil)% Complete:" -PercentComplete $percentil
+    Write-Progress -Activity "Reading data from servers..." -Status "$([int]$percentil)% Complete:" -PercentComplete $percentil -Id 1
   }
+  Write-Progress -Activity 'Reading data from servers...' -Completed -Id 1
   return $wmiValues
 }
 
 function Get-Invetary {
   Param($Out="C:\Work\MyInventory.xml", $Servers="C:\Users\pb003283\Documents\myservers.txt")
   $computers = Check-Servers -Servers (Get-Content $Servers)
+  if(!$computers) {
+    Exit
+  }
   $wmiData = Get-ServersInformation -Servers $computers
   [xml]$XML = New-Object System.Xml.XmlDocument
  
@@ -120,15 +124,21 @@ function Get-Invetary {
  
   #create a node for each computer
   $I = 0
+  #get a list of properties except PSComputername
+  $props = $wmiData.Win32_Computersystem | Get-Member -MemberType NoteProperty | where {$_.Name -ne 'PSComputername'} | select -ExpandProperty Name
+  $propsHD = $wmiData.Win32_DiskDrive | Get-Member -MemberType NoteProperty | where {$_.Name -ne 'PSComputername'} | select -ExpandProperty Name
+  $propsNetInterface = $wmiData.Win32_NetworkAdapter | Get-Member -MemberType NoteProperty | where {$_.Name -ne 'PSComputername'} | select -ExpandProperty Name
+  $propsService = $wmiData.Win32_Service | Get-Member -MemberType NoteProperty | where {$_.Name -ne 'PSComputername'} | select -ExpandProperty Name
+  $propsNetConf = $wmiData.Win32_NetworkAdapterConfiguration | Get-Member -MemberType NoteProperty | where {$_.Name -ne 'PSComputername'} | select -ExpandProperty Name
+
   foreach ($computer in $computers) {
     $I++
-    $percentil = ($I / $computers.Count) * 100
+    $percentil = ($I / $computers.Length) * 100
     Write-Progress -Activity "Creating XML for server $computer..." -Status "$([int]$percentil)% Complete:" -PercentComplete $percentil
 
     $c = $XML.CreateNode("element","Computer",$null)
     $pcID =  $wmiData.Win32_Computersystemproduct | where {$_.pscomputername -eq $Computer}
     #add an attribute for the name
-    $c.SetAttribute("Name",$computer) | Out-Null
     $c.SetAttribute("Id", $pcID.uuid)| Out-Null
     $c.SetAttribute("Type", $pcID.Name)| Out-Null
     $c.SetAttribute("Vendor", $pcID.Vendor)| Out-Null
@@ -156,37 +166,49 @@ function Get-Invetary {
     $c.AppendChild($osnode) | Out-Null
  
     #create node for Computer system
-    $csnode = $XML.CreateNode("element","ComputerSystem",$null)
-    $cshd = $XML.CreateNode("element","HDs",$null)
+    $csnode =      $XML.CreateNode("element","ComputerSystem",$null)
+    $cshd =        $XML.CreateNode("element","HDs",$null)
     $csInterface = $XML.CreateNode("element","NetworkInterfaces",$null)
     $csOpenPorts = $XML.CreateNode("element","OpenPorts",$null)
+    $svcnode =     $XML.CreateNode("element","Services",$null)
+
 
     #this is using the original property name
-    $data =  $wmiData.Win32_Computersystem | where {$_.pscomputername -eq $Computer}
-    $dataHDs =  $wmiData.Win32_DiskDrive | where {$_.pscomputername -eq $Computer}
-    $dataNetAdapters =  $wmiData.Win32_NetworkAdapter | where {$_.pscomputername -eq $Computer} | sort Name
-    $dataNetConfigs =  $wmiData.Win32_NetworkAdapterConfiguration | where {$_.pscomputername -eq $Computer}
-    $dataOpenPorts = (($wmiData.Win32_NetworkOpenPort | where {$_.pscomputername -eq $Computer} | Select-String -Pattern '\s+(TCP.*LISTENING|UDP)') -replace '^\s+', '') -replace '\s+',' '
- 
-    #get a list of properties except PSComputername
-    $props = ($wmiData.Win32_Computersystem[0] | Get-Member -MemberType Properties | where Name -ne 'PSComputername').Name
-    $propsHD = ($wmiData.Win32_DiskDrive[0] | Get-Member -MemberType Properties | where Name -ne 'PSComputername').Name
-    $propsNetInterface = ($wmiData.Win32_NetworkAdapter[0] | Get-Member -MemberType Properties | where Name -ne 'PSComputername').Name
-    $propsNetConf = ($wmiData.Win32_NetworkAdapterConfiguration[0] | Get-Member -MemberType Properties | where Name -ne 'PSComputername').Name
+    $data = $wmiData.Win32_Computersystem | where {$_.pscomputername -eq $Computer}
+    $dataHDs = $wmiData.Win32_DiskDrive | where {$_.pscomputername -eq $Computer}
+    $dataNetAdapters = $wmiData.Win32_NetworkAdapter | where {$_.pscomputername -eq $Computer}
+    $dataNetConfigs = $wmiData.Win32_NetworkAdapterConfiguration | where {$_.pscomputername -eq $Computer}
+    $dataService = $wmiData.Win32_Service | where {$_.pscomputername -eq $Computer}
+    $dataOpenPorts = (($wmiData.NetworkOpenPort | where {$_.pscomputername -eq $Computer} | Select-String -Pattern '\s+(TCP.*LISTENING|UDP)') -replace '^\s+', '') -replace '\s+',' '
 
     #create elements for each property
-    $props | Foreach {
-      $e = $XML.CreateNode("element",$_,$null)
-      $e.InnerText = $data.$_
+    if ($data.DomainRole -ge 3) {
+      $SId = $Computer | Get-ADComputer -Properties SID | select -ExpandProperty SID |select -ExpandProperty Value
+      $c.SetAttribute("SId", $SId)| Out-Null
+    }
+    foreach ($prop in $props) {
+      $e = $XML.CreateNode("element",$prop,$null)
+      if ($prop -eq "DomainRole") {
+        switch($data.$prop) {
+          0 {$e.InnerText = "Standalone Workstation"}
+          1 {$e.InnerText = "Member Workstation"}
+          2 {$e.InnerText = "Standalone Server"}
+          3 {$e.InnerText = "Member Server"}
+          4 {$e.InnerText = "Backup Domain Controller"}
+          5 {$e.InnerText = "Primary Domain Controller"}
+        }
+      } else {
+        $e.InnerText = $data.$prop
+      }
       $csnode.AppendChild($e) | Out-Null
     }
     #create hds elements
     foreach ($dataHD in $dataHDs) {
       $hdNode = $XML.CreateNode("element","HD",$null)
-      $propsHD | Foreach {
-        if ($dataHD.$_) {
-          $e = $XML.CreateNode("element",$_,$null)
-          $e.InnerText = $dataHD.$_
+      foreach ($propHD in $propsHD) {
+        if ($dataHD.$propHD) {
+          $e = $XML.CreateNode("element",$propHD,$null)
+          $e.InnerText = $dataHD.$propHD
           $hdNode.AppendChild($e) | Out-Null
         }
       }
@@ -195,20 +217,20 @@ function Get-Invetary {
     #create net adapter elements
     foreach($dataNetAdapter in $dataNetAdapters) {
       $netAdapterNode = $XML.CreateNode("element","NetworkInterface",$null)
-      $propsNetInterface | Foreach {
-      if ($dataNetAdapter.$_) {
-        $e = $XML.CreateNode("element",$_,$null)
-        $e.InnerText = $dataNetAdapter.$_
-        $netAdapterNode.AppendChild($e) | Out-Null
-       }
+      foreach($propNetInterface in $propsNetInterface) {
+        if ($dataNetAdapter.$propNetInterface) {
+          $e = $XML.CreateNode("element",$propNetInterface,$null)
+          $e.InnerText = $dataNetAdapter.$propNetInterface
+          $netAdapterNode.AppendChild($e) | Out-Null
+         }
       }
       $dataNetConfig = $dataNetConfigs | where {$_.MACAddress -eq $dataNetAdapter.MACAddress}
       if ($dataNetConfig) {
         $netAdapterConfNode = $XML.CreateNode("element","NetworkInterfaceConfig",$null)
-        $propsNetConf | Foreach {
-          if ($dataNetConfig.$_ -and !($_ -eq "MACAddress")) {
-            $e = $XML.CreateElement($_)
-            $e.InnerText = $dataNetConfig.$_
+        foreach($propNetConf in $propsNetConf) {
+          if ($dataNetConfig.$propNetConf -and !($propNetConf -eq "MACAddress")) {
+            $e = $XML.CreateNode("element",$propNetConf,$null)
+            $e.InnerText = $dataNetConfig.$propNetConf
             $netAdapterConfNode.AppendChild($e) | Out-Null
           }
         }
@@ -217,7 +239,7 @@ function Get-Invetary {
       $csInterface.AppendChild($netAdapterNode) | Out-Null
     }
     #create hash open ports
-    $hashOpenPorts = [ordered]@{}
+    $hashOpenPorts = New-Object System.Collections.Specialized.OrderedDictionary
     foreach($line in $dataOpenPorts) {
       $dt = $line -split ' '
       $protocol = $dt[0]
@@ -230,10 +252,10 @@ function Get-Invetary {
       }
       $pidOpen = $dt[-1]
       if (! $hashOpenPorts[$protocol]) {
-        $hashOpenPorts[$protocol] = [ordered]@{}
+        $hashOpenPorts[$protocol] = @{}
       }
       if (! $($hashOpenPorts[$protocol])[$ip]) {
-        $($hashOpenPorts[$protocol])[$ip] = [ordered]@{}
+        $($hashOpenPorts[$protocol])[$ip] = @{}
       }
       if (! $($($hashOpenPorts[$protocol])[$ip])[$port]) {
         $($($hashOpenPorts[$protocol])[$ip])[$port] = [String[]]@()
@@ -260,36 +282,24 @@ function Get-Invetary {
         }
         $csOpenPorts.AppendChild($csOpenProtocol) | Out-Null
     }
+    
+    foreach ($service in $dataService) {
+     $s = $XML.CreateNode("element","Service",$null)
+     foreach ($propService in $propsService) {
+       $e = $XML.CreateNode("element",$propService,$null)
+       $e.InnerText = $service.$propService
+       $s.AppendChild($e) | Out-Null
+     }
+     $svcnode.AppendChild($s) | Out-Null
+    }
 
-    #add to parent
     $csnode.AppendChild($cshd) | Out-Null
     $csnode.AppendChild($csInterface) | Out-Null
     $csnode.AppendChild($csOpenPorts) | Out-Null
     $c.AppendChild($csnode) | Out-Null
-
-    #create node for services
-    $svcnode = $XML.CreateNode("element","Services",$null)
- 
-    #get a list of properties except PSComputername
-    $props = ($wmiData.Win32_Service[0] | Get-Member -MemberType Properties | where Name -ne 'PSComputername').Name
-    $data =  $wmiData.Win32_Service | where({$_.pscomputername -eq $Computer})
-    foreach ($item in $data) {
-     #create a service node
-     $s = $XML.CreateNode("element","Service",$null)
-     #create elements for each property
-     $props | Foreach {
-       $e = $XML.CreateElement($_)
-       $e.InnerText = $item.$_
-       $s.AppendChild($e) | Out-Null
-     }
-     #add to parent
-     $svcnode.AppendChild($s) | Out-Null
-    }
-    #add to grandparent
     $c.AppendChild($svcnode) | Out-Null
-    #append to root
     $root.AppendChild($c) | Out-Null
-  } #foreach computer
+  }
   #add root to the XMLument
   $XML.AppendChild($root) | Out-Null
   #save file
