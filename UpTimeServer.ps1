088de2997a91edcacd9062799298f00f6972147b
Guilherme Namen Pimenta <guilherme.namen@gmail.com>
2018-08-03 13:18:35 -0300

    Better version

--- UpTimeServer.ps1
+++ UpTimeServer.ps1
@@ -1,58 +1,28 @@
-#---------------------------------------------------------------
-# CalculateSystemUpTimeFromEventLog.ps1
-# ed wilson, msft, 9/6/2008
-# 
-# Creates a system.TimeSpan object to subtract date values
-# Uses a .NET Framework class, system.collections.sortedlist to sort the events from eventlog.
-#
-#---------------------------------------------------------------
-#Requires -version 2.0
-Param($NumberOfDays = 30, [switch]$debug)
+function Get-ComputerUpTime {
+  #Requires -version 2.0
+  $startUpID = 6005
+  $shutDownID = 6006
+  $events = Get-WinEvent -FilterHashtable @{Logname='System';ID=@($startUpID,$shutDownID);ProviderName='EventLog'} | Sort-Object TimeCreated
+  $totalTime = $null
+  $now = Get-Date
 
-if($debug) { $DebugPreference = " continue" }
-
-[timespan]$uptime = New-TimeSpan -start 0 -end 0
-$currentTime = get-Date
-$startUpID = 6005
-$shutDownID = 6006
-$minutesInPeriod = (24*60)*$NumberOfDays
-$startingDate = (Get-Date -Hour 00 -Minute 00 -Second 00).adddays(-$numberOfDays)
-
-Write-debug "'$uptime $uptime" ; start-sleep -s 1
-write-debug "'$currentTime $currentTime" ; start-sleep -s 1
-write-debug "'$startingDate $startingDate" ; start-sleep -s 1
-
-$events = Get-EventLog -LogName system | 
-Where-Object { $_.eventID -eq  $startUpID -OR $_.eventID -eq $shutDownID `
-  -and $_.TimeGenerated -ge $startingDate } 
-#$events = Get-EventLog -LogName System -Source "Service Control Manager" | ? {$_.Message -like "The Apache Tomcat for BI 4 service entered the*" -and $_.TimeGenerated -ge $startingDate }
-
-write-debug "'$events $($events)" ; start-sleep -s 1
-
-$sortedList = New-object system.collections.sortedlist
-
-ForEach($event in $events)
-{
- $sortedList.Add( $event.timeGenerated, $event.eventID )
-} #end foreach event
-$uptime = $currentTime - $sortedList.keys[$($sortedList.Keys.Count-1)]
-Write-Debug "Current uptime $uptime"
-
-For($item = $sortedList.Count-2 ; $item -ge 0 ; $item -- )
-{ 
- Write-Debug "$item `t `t $($sortedList.GetByIndex($item)) `t `
-   $($sortedList.Keys[$item])" 
- if($sortedList.GetByIndex($item) -eq $startUpID)
- {
-  $uptime += ($sortedList.Keys[$item+1] - $sortedList.Keys[$item])
-  Write-Debug "adding uptime. `t uptime is now: $uptime"
- } #end if  
-} #end for item 
-
-"Total up time on $env:computername since $startingDate is " + "{0:n2}" -f `
-  $uptime.TotalMinutes + " minutes."
-$UpTimeMinutes = $Uptime.TotalMinutes
-$percentDownTime = "{0:n2}" -f (100 - ($UpTimeMinutes/$minutesInPeriod)*100)
-$percentUpTime = "{0:n2}" -f ( ($UpTimeMinutes/$minutesInPeriod)*100)
-
-"$percentDowntime% downtime and $percentUpTime% uptime."
+  $eventAfter = $null
+  $eventNow = $null
+  $eventFirst = $null
+  ForEach($event in $events) {
+    if ($event.Id -eq 6006) {
+      $eventNow = $event.TimeCreated
+    } elseif ($eventNow -ne $null -and $event.Id -eq 6005) {
+      $eventAfter = $event.TimeCreated
+    }
+    if ($eventAfter -ne $null -and $eventNow -ne $null) {
+      if ($eventFirst -eq $null) {
+        $eventFirst = $eventNow
+      }
+      $totalTime += $eventAfter - $eventNow
+      $eventAfter = $null
+      $eventNow = $null
+    }
+  }
+  return (100- (($totalTime.TotalSeconds / ($now - $eventFirst).TotalSeconds) *100))
+}
